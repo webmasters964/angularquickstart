3efecf5061e2dc29618b969bf6f9e27ae81ddfe8
Sanjay964 <sanjay.kumar@hotelbids.com>
2017-11-11 00:42:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Angular';
+  version: string;
+  year: any;
+  constructor() {
+    this.version = `v${VERSION.full}`;
+    var d = new Date();
+    this.year = d.getFullYear();
+  }
+  ngOnInit() {
+  }
+  
 }
